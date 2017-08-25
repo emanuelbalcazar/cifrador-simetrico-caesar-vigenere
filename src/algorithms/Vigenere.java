bb3c5ba7c7b293a12ac3a37c38280eb566a78d96
Emanuel <emanuelbalcazar13@gmail.com>
2017-08-25 17:20:33 +0000

    Cifrador Vigenere - Cifrado y Decifrado
    Se completo el cifrado y decifrado del cifrador Vigenere contemplando los espacios en el mensaje.

--- src/algorithms/Vigenere.java
+++ src/algorithms/Vigenere.java
@@ -1,6 +1,7 @@
 package algorithms;
 
 /**
+ * Cifrado Vigenere
  *
  * @author emanuel
  */
@@ -16,6 +17,14 @@ public class Vigenere implements Cipher {
 
     }
 
+    /**
+     * Cifra el mensaje utilizando cada letra de la clave para desplazar los
+     * caracteres
+     *
+     * @param password
+     * @param message
+     * @return mensaje cifrado.
+     */
     @Override
     public String code(String password, String message) {
         char[] buffer = message.toLowerCase().toCharArray();
@@ -23,18 +32,27 @@ public class Vigenere implements Cipher {
         int keyIndex = 0;
 
         for (int i = 0; i < buffer.length; i++) {
+            // contemplo los espacios, solo cifro los caracteres.
             if (buffer[i] != SPACE.charAt(0)) {
                 int messagePosition = getPosition(buffer[i]);
                 int keyPosition = getPosition(key[keyIndex]);
                 int result = ((messagePosition + keyPosition) % ALPHABET);
-                keyIndex++;
+
                 buffer[i] = (char) (result + 'a');
+                keyIndex++; // incremento la posicion del indice de la clave.
             }
         }
 
         return new String(buffer);
     }
-
+    
+    /**
+     * Decodifica un mensaje utilizando cada letra de la clave para desplazar los
+     * caracteres
+     * @param password
+     * @param message
+     * @return mensaje decifrado.
+     */
     @Override
     public String decode(String password, String message) {
         char[] buffer = message.toLowerCase().toCharArray();
@@ -42,29 +60,43 @@ public class Vigenere implements Cipher {
         int keyIndex = 0;
 
         for (int i = 0; i < buffer.length; i++) {
+            // contemplo los espacios, solo cifro los caracteres.
             if (buffer[i] != SPACE.charAt(0)) {
                 int messagePosition = getPosition(buffer[i]);
                 int keyPosition = getPosition(key[keyIndex]);
                 int result = 0;
-
+                // dependiendo el resultado de la resta, se realizan determinadas acciones.
                 if ((messagePosition - keyPosition) < 0) {
                     result = ((messagePosition - keyPosition + ALPHABET) % ALPHABET);
                 } else {
                     result = ((messagePosition - keyPosition) % ALPHABET);
                 }
-                keyIndex++;
 
                 buffer[i] = (char) (result + 'a');
+                keyIndex++;
             }
         }
 
         return new String(buffer);
     }
 
+    /**
+     * Obtiene la posicion de la letra en el abecedario.
+     *
+     * @param letter
+     * @return
+     */
     private int getPosition(char letter) {
         return letter - 'a';
     }
 
+    /**
+     * Retorna la clave del largo del mensaje indicado.
+     *
+     * @param password clave.
+     * @param length longitud del mensaje.
+     * @return clave concatenada sucesivamente.
+     */
     private char[] getKey(String password, int length) {
 
         while (password.length() < length) {
